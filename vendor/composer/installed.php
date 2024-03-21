46f31937e810bf33487d221505ffdf59d3f58f8d
GFUmair <164376583+GFUmair@users.noreply.github.com>
2024-03-21 23:04:40 +0500

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'cf1a7e75753d6124e384c8fe8f0db2e6384859c2',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'cf1a7e75753d6124e384c8fe8f0db2e6384859c2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
